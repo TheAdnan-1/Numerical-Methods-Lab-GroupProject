27208f538d5f0d45c3c326feac3a8797069d3d71
Mahabubul Alam <alam2207001@stud.kuet.ac.bd>
2025-12-13 21:48:55 +0600

    Newton's Forward Interpolation method implementation Done!

---
+++ src/InterpolationMethods/NewtonForward/NewtonForward.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define arr vector<double>
+double fun(const arr& coeff, double x) {
+    double result = 0;
+    int n = coeff.size(),i;
+    for (i=0; i < n; i++) result += coeff[i] * pow(x, n - i - 1);
+    return result;
+}
+int main() {
+    ifstream fin("input.txt");
+    ofstream fout("output.txt");
+    int t;
+    fin>>t;
+    while(t--){
+    int degree,i,n,j;
+    fin >> degree;
+    arr coeff(degree + 1);
+    for (i = 0; i <= degree; i++) fin >> coeff[i];
+    fin >> n;
+    arr x(n), y(n);
+    for (i = 0; i < n; i++) {
+        fin >> x[i];
+        y[i] = fun(coeff, x[i]);
+    }
+    double value;
+    fin >> value;
+    vector<arr> diff(n, arr(n));
+    for (i = 0; i < n; i++) diff[i][0] = y[i];
+    for (j = 1; j < n; j++) {
+        for (i = 0; i < n - j; i++) diff[i][j] = diff[i + 1][j - 1] - diff[i][j - 1];
+    }
+    double h = x[1] - x[0];
+    double u = (value - x[0]) / h;
+    double result = diff[0][0];
+    double u2 = 1;
+    double fact = 1;
+    for ( i = 1; i < n; i++) {
+        u2 *= (u - (i - 1));
+        fact *= i;
+        result += (u2 * diff[0][i]) / fact;
+    }
+    fout << "Interpolated value at x = " << value << " is " << result << endl;
+    }
+    fin.close();
+    fout.close();
+    return 0;
+}
