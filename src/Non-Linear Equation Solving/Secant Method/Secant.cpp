a56cba49f0170eec3a9321042adf2ecf5082019e
Rusho <losershit288@gmail.com>
2025-12-19 22:21:33 +0600

    Refactor Secant method implementation for clarity

--- src/Non-Linear Equation Solving/Secant Method/Secant.cpp
+++ src/Non-Linear Equation Solving/Secant Method/Secant.cpp
@@ -4,68 +4,42 @@ using namespace std;
 int degree;
 double coeff[20];
 
-// Polynomial function
-double f(double x)
-{
-    double result = 0;
-    for (int i = 0; i <= degree; i++)
-    {
-        result += coeff[i] * pow(x, degree - i);
-    }
+double f(double x){
+    double result=0;
+    for(int i=0;i<=degree;i++) result+=coeff[i]*pow(x,degree-i);
     return result;
 }
 
-int main()
-{
+int main(){
+    ifstream fin("input.txt");
+    ofstream fout("output.txt");
+
     int t;
-    cin >> t; // number of test cases
+    fin>>t;
+    while(t--){
+        double x0,x1,x2,tol;
+        int maxIter,iter=0;
+        fin>>degree;
+        for(int i=0;i<=degree;i++) fin>>coeff[i];
+        fin>>x0>>x1>>tol>>maxIter;
 
-    while (t--)
-    {
-        double x0, x1, x2, tol;
-        int maxIter, iter = 0;
-
-        // Input polynomial
-        cin >> degree;
-        for (int i = 0; i <= degree; i++)
-        {
-            cin >> coeff[i];
-        }
-
-        // Input parameters
-        cin >> x0 >> x1;
-        cin >> tol;
-        cin >> maxIter;
-
-        bool found = false;
-
-        while (iter < maxIter)
-        {
-            if (f(x1) - f(x0) == 0)
-            {
+        bool found=false;
+        while(iter<maxIter){
+            if(f(x1)-f(x0)==0) break;
+            x2=x1-(f(x1)*(x1-x0))/(f(x1)-f(x0));
+            if(fabs(x2-x1)<tol){
+                fout<<fixed<<setprecision(6);
+                fout<<"Root: "<<x2<<'\n';
+                found=true;
                 break;
             }
-
-            // Secant formula
-            x2 = x1 - (f(x1) * (x1 - x0)) / (f(x1) - f(x0));
-
-            if (fabs(x2 - x1) < tol)
-            {
-                cout << "Root: " << x2 << endl;
-                found = true;
-                break;
-            }
-
-            x0 = x1;
-            x1 = x2;
+            x0=x1;
+            x1=x2;
             iter++;
         }
-
-        if (!found)
-        {
-            cout << "Not Converged" << endl;
-        }
+        if(!found) fout<<"Not Converged\n";
     }
-
+    fin.close();
+    fout.close();
     return 0;
 }
