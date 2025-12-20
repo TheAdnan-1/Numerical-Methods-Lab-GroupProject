4aa39bae270ab46a88b9f2d0642f4d6067bdaf5b
Rusho <losershit288@gmail.com>
2025-12-20 21:18:42 +0600

    Refactor Bisection method for clarity and precision

--- src/Non-Linear Equation Solving/Bisection/Bisection.cpp
+++ src/Non-Linear Equation Solving/Bisection/Bisection.cpp
@@ -1,23 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int degree;
 double coeff[20];
-
 double f(double x){
     double result=0;
-    for(int i=0;i<=degree;i++) result+=coeff[i]*pow(x,degree-i);
+    for(int i=0;i<=degree;i++)
+        result+=coeff[i]*pow(x,degree-i);
     return result;
 }
-
 int main(){
     ifstream fin("input.txt");
     ofstream fout("output.txt");
-
-    double a,b,mid;
+    double a,b,mid=0;
     int n;
     fin>>degree;
-    for(int i=0;i<=degree;i++) fin>>coeff[i];
+    for(int i=0;i<=degree;i++)
+        fin>>coeff[i];
     fin>>a>>b>>n;
     if(f(a)*f(b)>=0){
         fout<<"Invalid interval\n";
@@ -25,12 +23,15 @@ int main(){
     }
     for(int i=0;i<n;i++){
         mid=(a+b)/2;
-        if(f(mid)==0) break;
-        else if(f(a)*f(mid)<0) b=mid;
-        else a=mid;
+        if(f(mid)==0)
+            break;
+        else if(f(a)*f(mid)<0)
+            b=mid;
+        else
+            a=mid;
     }
-    fout<<fixed<<setprecision(6)<<mid<<'\n';
-
+    fout<<fixed<<setprecision(6);
+    fout<<"Root: "<<mid<<endl;
     fin.close();
     fout.close();
     return 0;
